fe2f3fd2e86949e888782e880083fcb45e96d4b6
Christina Bui <uyen.buitie@gmail.com>
2023-05-23 16:27:57 -0500

    Style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h2>Our Clients Presents</h2>
+      <h1>Transformative Landscaping and Lawn Services</h1>
+      <h3>We are the best lawn and landscaping in Springfield MO</h3>
+      <p>Paragraph, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+      <button>Get a Quote</button>
+      <br></br>
+      <a>Call Us (563) 265-9641</a>
+
+      
     </div>
   );
 }
